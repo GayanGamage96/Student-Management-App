eb953453672ff0afa23efe0f8f29fafa9c9a59ce
yasindu1997 <yasindusathsara@gmail.com>
2023-03-04 22:57:32 +0530

    api completed

--- controller/student-controller.js
+++ controller/student-controller.js
@@ -8,19 +8,31 @@ const addStudent = (req, res) => {
 }
 
 const updateStudent = (req, res) => {
-
+    connection.query('UPDATE student set name=?, address=?, registered_date=?, course=?, image=? WHERE std_id=?', [req.body.name, req.body.address, req.body.registered_date, req.body.course, req.body.image, req.params.id], (err, rows) => {
+        if (err) throw err
+        res.json(rows)
+    })
 }
 
 const deleteStudent = (req, res) => {
-
+    connection.query('DELETE FROM student WHERE std_id=?', [req.params.id], (err, rows) => {
+        if (err) throw err
+        res.json(rows)
+    })
 }
 
 const getAllStudent = (req, res) => {
-
+    connection.query('SELECT * FROM student', (err, rows) => {
+        if (err) throw err
+        res.json(rows)
+    })
 }
 
 const getStudentById = () => {
-
+    connection.query('SELECT * FROM student WHERE std_id=?', [req.params.id], (err, rows) => {
+        if (err) throw err
+        res.json(rows)
+    })
 }
 
 module.exports = {
